02ddc1c43b9c8de2817a4c6307187446ba8385d3
Angelina <angelina.v.gegerfelt@gmail.com>
2016-11-12 13:50:04 +0100

    added keypress and first code for commands... however, not tested and working

--- challenge1/client/src/commands.js
+++ challenge1/client/src/commands.js
@@ -1,5 +1,8 @@
+var keypress = require('keypress');
+
 const sendCommand = command => {
 	//TODO socket stuff for sending commands
+	console.log(command);
 }
 
 const createCommand = charName => {
@@ -22,3 +25,32 @@ const scanCommand = CharName => {
 	//{"command": "scan", "name": <charName>}
 	return JSON.stringify({command: "scan", name: CharName});
 }
+
+
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  	console.log('got "keypress"', key);
+  	if (key && key.name == 'q') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', -1, -1))
+  	} else if (key && key.name == 'w') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', 0, -1))
+  	} else if (key && key.name == 'e') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', 1, -1))
+  	} else if (key && key.name == 'a') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', -1, 0))
+  	} else if (key && key.name == 'd') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', 1, 0))
+  	} else if (key && key.name == '<') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', -1, 1))
+  	} else if (key && key.name == 'z') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', 0, 1))
+  	} else if (key && key.name == 'x') {
+		sendCommand(moveCommand('INSERT_CHAR_NAME', 1, 1))
+  	} else if (key && key.name == 's') {
+		sendCommand(scanCommand('INSERT_CHAR_NAME'))
+  	} else if (key && key.name == 'c') {
+		sendCommand(createCommand('INSERT_CHAR_NAME'))
+  	} 
+});
