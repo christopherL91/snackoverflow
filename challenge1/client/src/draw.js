91cf4cbb8ea3ffa42f8ff2484a7fe0edd976a2fe
Angelina <angelina.v.gegerfelt@gmail.com>
2016-11-12 17:59:52 +0100

    changed our char to green

--- challenge1/client/src/draw.js
+++ challenge1/client/src/draw.js
@@ -1,4 +1,4 @@
-export const drawSquare = (position, mult=20, fillstyle="red") => {
+export const drawSquare = (position, mult=20, fillstyle="green") => {
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext('2d');
 	ctx.fillStyle = fillstyle;
