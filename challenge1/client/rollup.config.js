e47eb14f1eb352d21e7d9f730bb64b5d71353c5c
Christopher Lillthors <christopher.lillthors@gmail.com>
2016-11-12 13:52:46 +0100

    Remove socket.io

--- challenge1/client/rollup.config.js
+++ challenge1/client/rollup.config.js
@@ -1,8 +1,26 @@
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: 'src/main.js',
+    moduleName: 'snackoverflow',
+    plugins: [
+        nodeResolve({
+            jsnext: true,
+            main: true,
+            browser: true,
+            extensions: ['.js', '.json']
+        }),
+        commonjs({
+            include: 'node_modules/**',
+            extensions: ['.js', '.coffee'],
+            sourceMap: true,
+            ignoreGlobal: true,
+        }),
+    ],
     format: 'iife',
     plugins: [babel()],
-    dest: 'index.js'
+    dest: 'index.js',
+    sourceMap: 'inline',
 };
